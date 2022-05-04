e1e564b62f94dc5e1c28bd305478e14436ce64a8
Oscar Chan <oscarchan@asl.com.hk>
2022-05-04 14:33:43 +0800

    feat: rename usersApi

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import store from './app/store'
 import { Provider } from 'react-redux'
 
 import { worker } from './api/server'
-import { extendedApiSlice } from './features/users/usersSlice'
+import { usersApi } from './features/users/usersSlice'
 
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
 
-  store.dispatch(extendedApiSlice.endpoints.getUsers.initiate())
+  store.dispatch(usersApi.endpoints.getUsers.initiate())
 
   ReactDOM.render(
     <React.StrictMode>
