d767a42fcbaf9952ca916709d127ee00c5a2e253
Oscar Chan <oscarchan@asl.com.hk>
2022-05-05 10:10:58 +0800

    feat: handle error

--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import { postsApi } from './postsSlice'
@@ -8,48 +8,69 @@ import { Spinner } from '../../components/Spinner'
 export const EditPostForm = ({ match }) => {
   const { postId } = match.params
 
-  const { data: post } = postsApi.useGetPostQuery(postId)
-  const [updatePost, { isLoading }] = postsApi.useEditPostMutation()
+  const {
+    data: post,
+    isSuccess,
+    isLoading,
+    isError,
+    error,
+  } = postsApi.useGetPostQuery(postId)
+  const [updatePost, { isLoading: isSaving }] = postsApi.useEditPostMutation()
 
-  const [title, setTitle] = useState(post.title)
-  const [content, setContent] = useState(post.content)
+  const [postTitle, setPostTitle] = useState(post?.title)
+  const [postContent, setPostContent] = useState(post?.content)
 
   const history = useHistory()
 
-  const onTitleChanged = (e) => setTitle(e.target.value)
-  const onContentChanged = (e) => setContent(e.target.value)
+  const onPostTitleChanged = (e) => setPostTitle(e.target.value)
+  const onPostContentChanged = (e) => setPostContent(e.target.value)
 
   const onSavePostClicked = async () => {
-    if (title && content) {
-      await updatePost({ id: postId, title, content })
+    if (postTitle && postContent) {
+      await updatePost({ id: postId, title: postTitle, content: postContent })
       history.push(`/posts/${postId}`)
     }
   }
-  return (
-    <section>
-      <h2>Edit Posts</h2>
-      <form>
-        <label htmlFor="postTitle">Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          value={title}
-          placeholder="What's on your mind"
-          onChange={onTitleChanged}
-        ></input>
-        <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChanged}
-        ></textarea>
-        <button type="button" onClick={onSavePostClicked} disabled={isLoading}>
-          Save Post
-        </button>
-        {isLoading && <Spinner text="Saving..." />}
-      </form>
-    </section>
-  )
+
+  let content
+
+  if (isLoading) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
+      <Fragment>
+        <h2>Edit Posts</h2>
+        <form>
+          <label htmlFor="postTitle">Title:</label>
+          <input
+            type="text"
+            id="postTitle"
+            name="postTitle"
+            value={postTitle}
+            placeholder="What's on your mind"
+            onChange={onPostTitleChanged}
+          ></input>
+          <label htmlFor="postContent">Content:</label>
+          <textarea
+            id="postContent"
+            name="postContent"
+            value={postContent}
+            onChange={onPostContentChanged}
+          ></textarea>
+          <button type="button" onClick={onSavePostClicked} disabled={isSaving}>
+            Save Post
+          </button>
+          {isSaving && <Spinner text="Saving..." />}
+        </form>
+      </Fragment>
+    )
+  } else if (isError) {
+    content = (
+      <h2>
+        {error.data.errorType}: {error.data.message}
+      </h2>
+    )
+  }
+
+  return <section>{content}</section>
 }
